f6553fb555f96da144367975e5916857daa73a65
OmarAlex24 <artcate208@gmail.com>
2024-10-31 14:03:09 -0600

    feat: agregar interfaces necesarias para mostrar productos

--- src/types.tsx
+++ src/types.tsx
@@ -8,3 +8,47 @@ export const STYLES = {
   NORMAL: 'normal',
   OUTLINE: 'outline',
 } as const;
+
+export interface Studio {
+  id: number;
+  studioName: string;
+  email: string;
+  phone: string;
+  description: string;
+  signup: Date;
+  yearsOfExperience: number;
+  profileImage: string;
+  location: Location;
+  photographers: Photographer[];
+  portfolioPhotos: PortfolioPhoto[];
+  studioSpecialties: StudioSpecialty[];
+}
+
+export interface Location {
+  id: number;
+  city: string;
+  state: string;
+  country: string;
+  address: string;
+}
+
+export interface Photographer {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+export interface PortfolioPhoto {
+  id: number;
+  image: string;
+}
+
+export interface StudioSpecialty {
+  id: number;
+  specialty: Specialty;
+}
+
+export interface Specialty {
+  id: number;
+  specialtyName: string;
+}
