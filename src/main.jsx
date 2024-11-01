361e1f97f60d585f9ef76fe83682305b18c2067d
OmarAlex24 <artcate208@gmail.com>
2024-10-31 19:28:43 -0600

    fix: corregir la importación de store eliminando la extensión de archivo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import store from './store.js';
+import store from './store';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 
