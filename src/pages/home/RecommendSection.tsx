50ca6d118ce079bf3f1c09699d0d202bb72a8940
OmarAlex24 <artcate208@gmail.com>
2024-11-01 13:39:58 -0600

    refactor: ajustar el espaciado en RecommendSection para mejorar la presentación de las recomendaciones

--- src/pages/home/RecommendSection.tsx
+++ src/pages/home/RecommendSection.tsx
@@ -37,7 +37,7 @@ const RecommendSection = () => {
           Recomendaciones para ti
         </h3>
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-8">
             {currentItems.map((studio) => (
               <div key={studio.id}>
                 <CardRecommend studio={studio} />
