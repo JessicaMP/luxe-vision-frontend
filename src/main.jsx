d8e61e11322ad3f47705945a2cf5d0f76f8cc7d2
OmarAlex24 <artcate208@gmail.com>
2024-11-01 08:30:26 -0600

    feat: agregar persistencia a Redux utilizando redux-persist en el store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import store from './store';
+import store, { persistor } from './store';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
