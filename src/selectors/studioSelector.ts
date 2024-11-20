2a7db317c4e301bdaaeead330f8b07e741f9257a
OmarAlex24 <artcate208@gmail.com>
2024-11-20 08:41:40 -0600

    Refactor: Update import path for studioSelector in Detail.tsx

--- src/selectors/studioSelector.ts
+++ src/selectors/studioSelector.ts
@@ -1,23 +1,16 @@
 import { RootState } from '@/store';
 import { Studio } from '@/types';
 import { createSelector } from '@reduxjs/toolkit';
-import { setActiveStudio } from '../reducers/studioReducer';
 
 export const selectStudios = (state: RootState) => state.studios.studios;
 
-export const selectStudioById = createSelector(
-  [(state: RootState) => state.studios.studios, (_, studioId: number, dispatch?: any) => ({ studioId, dispatch })],
-  (studios, { studioId, dispatch }) => {
-    const studio = studios.find((studio: Studio) => studio.id === studioId);
-    if (studio && dispatch) {
-      dispatch(setActiveStudio(studio));
-    }
-    return studio;
-  }
+export const selectStudioById = () => createSelector(
+  [(state: RootState) => state.studios.studios, 
+   (_: RootState, studioId: number) => studioId],
+  (studios, studioId) => studios.find((studio: Studio) => studio.id === studioId)
 );
 
 export const selectFeatures = (state: RootState) => state.features.features;
 export const selectSpecialties = (state: RootState) => state.specialties.specialties;
 export const selectStudio = (state: RootState) => state.studios.studio;
-
 export const selectStudioStatus = (state: RootState) => state.studios.status;
