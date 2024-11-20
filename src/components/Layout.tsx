1e3ee8d667d943a0c9c4a38670309e3a16b32553
OmarAlex24 <artcate208@gmail.com>
2024-11-20 08:40:38 -0600

    Refactor: Update Layout component to fetch studios and fix layout width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,23 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { useLocation } from "react-router-dom";
 import { IoWarningOutline } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { selectStudios, selectStudioStatus } from "@/selectors/studioSelector";
+import { useEffect } from "react";
+import { fetchStudios } from "@/reducers/studioReducer";
+import { AppDispatch } from "@/store";
 
 const Layout = ({ children }: any) => {
   const location = useLocation();
+  const studios = useSelector(selectStudios);
+  const status = useSelector(selectStudioStatus);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (status === "idle" && studios.length === 0) {
+      dispatch(fetchStudios());
+    }
+  }, [dispatch, studios.length, status]);
 
   const isAdminRoute = location.pathname.includes("administration");
   if (isAdminRoute) {
@@ -14,7 +28,7 @@ const Layout = ({ children }: any) => {
         <Header isLogin={true} />
         <main className="bg-[#454243] hidden xl:flex pt-20 relative">
           <SideBar />
-          <div className="ml-52">{children}</div>
+          <div className="ml-52 w-full">{children}</div>
         </main>
         <section className="bg-[#454243] flex flex-col justify-center items-center xl:hidden pt-28 h-screen text-white gap-4 pl-20">
           <IoWarningOutline className="text-6xl" />
