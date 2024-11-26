063e1c77beec9ac3b54f2d39f6576491541b1e0b
OmarAlex24 <artcate208@gmail.com>
2024-11-26 14:38:47 -0600

    Refactor: Simplify selectStudioById selector in studioSelector.ts

--- src/selectors/studioSelector.ts
+++ src/selectors/studioSelector.ts
@@ -1,15 +1,10 @@
 import { RootState } from '@/store';
 import { Studio } from '@/types';
-import { createSelector } from '@reduxjs/toolkit';
-import { setActiveStudio } from '../reducers/studiosReducer';
 
 export const selectStudios = (state: RootState) => state.studios.studios;
 
-export const selectStudioById = () => createSelector(
-  [(state: RootState) => state.studios.studios, 
-   (_: RootState, studioId: number) => studioId],
-  (studios, studioId) => studios.find((studio: Studio) => studio.id === studioId)
-);
+export const selectStudioById = (id: number, state: RootState) =>
+  state.studios.studios.find((studio: Studio) => studio.id === id);
 
 export const selectFeatures = (state: RootState) => state.features.features;
 export const selectSpecialties = (state: RootState) => state.specialties.specialties;
