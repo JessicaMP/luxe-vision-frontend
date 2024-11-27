52131fa15a1620f163f7c46a18921114ff96dc3e
OmarAlex24 <artcate208@gmail.com>
2024-11-27 16:16:50 -0600

    Refactor: Disable MillionLint plugin in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import MillionLint from '@million/lint';
-import path from 'path';
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import MillionLint from "@million/lint";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [MillionLint.vite({
-    enabled: true
-  }), react()],
+  plugins: [
+    MillionLint.vite({
+      enabled: false,
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
