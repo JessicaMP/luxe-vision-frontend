aafede3e4181536e79ac8371699baaf5493936bd
OmarAlex24 <artcate208@gmail.com>
2024-11-28 20:36:27 -0600

    Refactor: Add fetchAllFeatures and fetchSpecialties actions to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { fetchStudiosAPI } from "./reducers/studiosReducer";
+import { fetchAllFeatures } from "./reducers/featuresReducer";
+import { fetchSpecialties } from "./reducers/specialtiesReducer";
 
 store.dispatch(fetchStudiosAPI());
+store.dispatch(fetchAllFeatures());
+store.dispatch(fetchSpecialties());
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
