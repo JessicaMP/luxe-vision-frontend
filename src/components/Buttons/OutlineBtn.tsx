4f04a0ac7e23415c4230d3fdfbf8f59b0aa677b8
OmarAlex24 <artcate208@gmail.com>
2024-11-29 17:48:06 -0600

    Fix: NormalBtn component to use react-router-dom for navigation

--- src/components/Buttons/OutlineBtn.tsx
+++ src/components/Buttons/OutlineBtn.tsx
@@ -1,21 +1,23 @@
 import Button from "@mui/joy/Button";
-// import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const OutlineBtn = ({ nombre }) => {
+  const navigate = useNavigate();
+
   return (
     <Button
       variant="outlined"
       size="lg"
       sx={{
-        color: 'white',
-        borderColor: '#FFA987CC',
-        borderRadius: '10px',
-        '&:hover': {
-          borderColor: '#FFA987CC',
-          backgroundColor: 'rgba(255, 169, 135, 0.1)',
+        color: "white",
+        borderColor: "#FFA987CC",
+        borderRadius: "10px",
+        "&:hover": {
+          borderColor: "#FFA987CC",
+          backgroundColor: "rgba(255, 169, 135, 0.1)",
         },
       }}
-      href="/register"
+      onClick={() => navigate("/register")}
       component="a"
     >
       {nombre}
