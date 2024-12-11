e37d421c9bc7b98facb392f2943b22b50aed83fc
OmarAlex24 <artcate208@gmail.com>
2024-12-11 01:26:16 -0600

    Refactor: Update import path for Studio type in studioSelector

--- src/selectors/studioSelector.ts
+++ src/selectors/studioSelector.ts
@@ -1,5 +1,5 @@
 import { RootState } from '@/store';
-import { Studio } from '@/types';
+import { Studio } from '@/types/studio';
 
 export const selectStudios = (state: RootState) => state.studios.studios;
 
