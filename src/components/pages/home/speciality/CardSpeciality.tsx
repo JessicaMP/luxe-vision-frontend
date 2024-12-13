09a099a325485efc50361553313ec325b9ca5aa0
OmarAlex24 <artcate208@gmail.com>
2024-12-12 21:15:26 -0600

    refactor: deleted static specialties

--- src/components/pages/home/speciality/CardSpeciality.tsx
+++ src/components/pages/home/speciality/CardSpeciality.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 interface CardProps {
   title: string;
@@ -6,22 +6,19 @@ interface CardProps {
   className?: string;
 }
 
-export default function CardSpeciality({
-  title = 'Bodas',
-  image = '/placeholder.svg?height=400&width=300',
-}: CardProps) {
+export default function CardSpeciality({ title, image }: CardProps) {
   return (
-    <Card className="smm:max-w-[250px] overflow-hidden bg-[#323232] text-white border-[#323232] border-8 border-y-8">
+    <Card className="smm:max-w-[250px] overflow-hidden bg-[#323232] text-white border-[#323232] border-8 border-y-8 h-full">
       <CardContent className="p-0">
         <div className="relative aspect-square">
           <img
-            alt="Product photography setup"
+            alt={title}
             className="object-cover"
             height="300"
             src={image}
             style={{
-              aspectRatio: '300/300',
-              objectFit: 'cover',
+              aspectRatio: "300/300",
+              objectFit: "cover",
             }}
             width="300"
           />
