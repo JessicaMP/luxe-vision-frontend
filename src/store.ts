9d48a71fbed97be2ffd6124ad5793fd17845c76a
OmarAlex24 <artcate208@gmail.com>
2024-12-12 23:01:58 -0600

    refactor: add expiration time of store

--- src/store.ts
+++ src/store.ts
@@ -10,42 +10,94 @@ import authSlice from '@/reducers/authReducer';
 import favoritesReducer from './reducers/favoritesReducer';
 import favoritesMiddleware from "@/middleware/favorites";
 import bookingsReducer from './reducers/bookingReducer';
+import { createTransform } from 'redux-persist';
+
+const EXPIRATION_TIME = 4 * 60 * 60 * 1000; // 4 horas en milisegundos
+
+const checkExpirationMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  const state = store.getState();
+
+  // Verificar timestamps y dispatch acciones de recarga si es necesario
+  Object.keys(state).forEach((key) => {
+    if (state[key]?._timestamp) {
+      const now = Date.now();
+      if (now - state[key]._timestamp > EXPIRATION_TIME) {
+        // Dispatch acciones para recargar datos
+        store.dispatch({ type: `${key}/reload` });
+      }
+    }
+  });
+
+  return result;
+};
+
+const createExpirationTransform = (expirationTime: number) => {
+  return createTransform(
+    (inboundState) => {
+      return {
+        ...inboundState,
+        _timestamp: Date.now(),
+      };
+    },
+    (outboundState) => {
+      if (outboundState && outboundState._timestamp) {
+        const now = Date.now();
+        if (now - outboundState._timestamp > EXPIRATION_TIME) {
+          return undefined;
+        }
+        const { _timestamp, ...state } = outboundState;
+        return state;
+      }
+      return outboundState;
+    }
+  );
+};
+
+const expirationTransform = createExpirationTransform(EXPIRATION_TIME);
 
 const authPersistConfig = {
   key: 'users',
   storage,
   whitelist: ["token", "user"],
+  transforms: [expirationTransform],
 };
 
 const studioPersistConfig = {
   key: "studios",
   storage,
+  transforms: [expirationTransform],
 };
 
 const featurePersistConfig = {
   key: "features",
   storage,
+  transforms: [expirationTransform],
 };
 
 const featureSpecialtiesConfig = {
   key: "specialties",
   storage,
+  transforms: [expirationTransform],
 };
 
 const favoritePersistConfig = {
   key: "favorites",
   storage,
+  transforms: [expirationTransform],
   whitelist: ["favorites"],
 };
 
 const availabilityPersistConfig = {
   key: "availability",
   storage,
+  transforms: [expirationTransform],
 };
 
 const bookingsPersistConfig = {
   key: "bookings",
   storage,
+  transforms: [expirationTransform],
 };
 
 const persistedStudiosReducer = persistReducer(studioPersistConfig, studiosReducer);
@@ -79,7 +131,7 @@ const store = configureStore({
           "persist/REGISTER",
         ],
       },
-    }).concat(favoritesMiddleware),
+    }).concat(favoritesMiddleware, checkExpirationMiddleware),
 });
 
 export const persistor = persistStore(store);
